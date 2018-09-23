c336b1ae520c1476047f39bd26d98af59f4679f8
Kyle Sands <kyle@lightburn.co>
2018-09-22 23:08:06 -0500

    Startup shit

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chambers.Models;
+
+namespace Chambers.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Character LeftArbiter { get; set; }
+        public Character RightArbiter { get; set; }
+        public Character LeftVictim { get; set; }
+        public Character RightVictim { get; set; }
+
+        public Affliction LeftArbiterAfflication { get; set; }
+        public Affliction RightArbiterAffliction { get; set; }
+        public Affliction LeftVictimAffliction { get; set; }
+        public Affliction RightVictimAffliction { get; set; }
+
+        public bool UseSelflessness { get; set; }
+        public bool UseFrustation { get; set; }
+    }
+}
